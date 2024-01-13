515f1e008a2542be4255fa96ada83d32a19dbb29
Ceno <ceno.e@gmail.com>
2024-01-14 02:33:31 +0800

    first commit

---
+++ app/src/main/java/com/example/lingapp/ui/LoginPage/LoginPageActivity.java
@@ -0,0 +1,17 @@
+
+package com.example.lingapp.ui.LoginPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lingapp.R;
+
+public class LoginPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
