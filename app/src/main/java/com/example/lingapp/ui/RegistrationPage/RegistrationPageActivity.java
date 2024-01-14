dcddcae320cf13ab11005f955c7371f94cbad7f5
Ceno <ceno.e@gmail.com>
2024-01-14 10:09:41 +0800

    designed the login and registration page

---
+++ app/src/main/java/com/example/lingapp/ui/RegistrationPage/RegistrationPageActivity.java
@@ -0,0 +1,16 @@
+package com.example.lingapp.ui.RegistrationPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lingapp.R;
+
+public class RegistrationPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_page);
+    }
+}
